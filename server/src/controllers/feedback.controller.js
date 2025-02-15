8926a03574839a6c83f7de977d572eb32877dc6d
Nitesh985 <Niteshacharya796@gmail.com>
2025-02-15 16:51:41 +0545

    changes made

---
+++ server/src/controllers/feedback.controller.js
@@ -0,0 +1,11 @@
+
+
+const writeFeedback = asyncHandler(async(req, res)=>{
+    const {orderId, }
+})
+
+const getAllFeedbacks = asyncHandler(async(req, res)=>{
+
+})
+
+export { writeFeedback, getAllFeedbacks }
---
+++ server/src/models/feedback.models.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose'
+
+const feedbackSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    orderId:{
+        type: Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    message:{
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+export const Feedback = model("Feedback", feedbackSchema)
