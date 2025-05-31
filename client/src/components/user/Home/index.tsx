449ac78dd701d9fa215bf35a3adcb9f4a091ba8b
Nitesh <107662492+Nitesh985@users.noreply.github.com>
2025-05-31 23:06:56 +0545

    Update index.tsx

--- client/src/components/user/Home/index.tsx
+++ client/src/components/user/Home/index.tsx
@@ -42,9 +42,9 @@ function Home() {
   return (
     <main className="w-full">
       <div className="border border-t border-white border-opacity-20"></div>
-      <div className="sticky border-b-slate-800 top-0 scrollbar-hide flex h-28  items-center space-x-5 overflow-auto bg-white z-20">
+      <div className="flex justify-center items-center"><div className="sticky w-3/4 border-b-slate-800 top-0 scrollbar-hide flex h-28  items-center space-x-5 overflow-auto bg-white z-20">
         <Categories />
-      </div>
+      </div></div>
       <div className="border border-t border-white border-opacity-20 z-10"></div>
       <div className="divider"></div>
       <section className="-mx-5 flex flex-wrap justify-between gap-y-14 py-6 px-4">
